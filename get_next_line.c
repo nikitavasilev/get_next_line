68641b1660afe12234ac947112d10a16e9600a90
Nikita VASILEV <nvasilev@student.42.fr>
2021-01-12 15:59:14 +0100

    tests on read bytes

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/06 00:37:56 by nvasilev          #+#    #+#             */
-/*   Updated: 2021/01/12 15:27:39 by nvasilev         ###   ########.fr       */
+/*   Updated: 2021/01/12 15:56:12 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -89,10 +89,10 @@ int		get_next_line(int fd, char **line)
 	char	buf[BUFFER_SIZE + 1];
 	int		byte_was_read;
 	char	*p_n;
-	int		flag;
+	int		is_newline;
 	static char *remainder;
 
-	flag = 1;
+	is_newline = 0;
 	if (remainder)
 		*line = ft_strdup(remainder);
 	else
@@ -101,18 +101,20 @@ int		get_next_line(int fd, char **line)
 			return (0);
 		line[0][0] = '\0';
 	}
-	while (flag && (byte_was_read = read(fd, buf, BUFFER_SIZE)))
+	printf("%s\n", remainder);
+	while (!is_newline && (byte_was_read = read(fd, buf, BUFFER_SIZE)))
 	{
 		if ((p_n = ft_strchr(buf, '\n')))
 		{
 			*p_n = '\0';
-			flag = 0;
+			is_newline = 1;
 			p_n++;
 			remainder = ft_strdup(p_n);
 		}
 		buf[byte_was_read] = '\0';
 		*line = ft_strjoin(*line, buf);
 	}
+	printf("%d\n", byte_was_read);
 	return (0);
 }
 
@@ -122,6 +124,10 @@ int		main(void)
 	int		fd;
 
 	fd = open("file.txt", O_RDONLY);
+	get_next_line(fd, &line);
+	printf("%s\n", line);
+	get_next_line(fd, &line);
+	printf("%s\n", line);
 	get_next_line(fd, &line);
 	printf("%s\n", line);
 	get_next_line(fd, &line);
